e3451e842af4f0cae5465ad3abaa9dc4e5f75e77
edwinhuish <edwinhuish@gmail.com>
2025-12-31 12:05:29 +0800

    feat: 支持 field mapper

--- adapter/ent/ent.go
+++ adapter/ent/ent.go
@@ -7,15 +7,51 @@ import (
 	"github.com/epkgs/query/clause"
 )
 
+type options struct {
+	mapper MapperFunc
+}
+
+type Option func(*options)
+
+// MapperFunc 定义了查询列名到 ENT 列名的映射函数类型
+// 输入参数 queryColumn 是查询条件中使用的列名
+// 返回值 entColumn 是数据库表中的实际列名
+type MapperFunc func(queryColumn string) (entColumn string)
+
+// WithMapper 创建一个配置选项，用于设置查询列名到数据库实际列名的映射函数
+// mapper 参数是一个 MapperFunc 类型的函数，用于自定义列名映射逻辑
+// 该选项用于在将查询条件转换为 ENT 条件时，通过自定义函数实现灵活的列名映射
+// 例如：可以实现驼峰命名转下划线命名、添加前缀/后缀等复杂映射逻辑
+func WithMapper(mapper MapperFunc) Option {
+	// 返回一个 Option 函数类型，该函数接收一个 options 指针并修改其 mapper 字段
+	return func(o *options) {
+		o.mapper = mapper
+	}
+}
+
+func (o *options) Column(column string) string {
+	if o.mapper != nil {
+		return o.mapper(column)
+	}
+
+	return column
+}
+
 // Where 将 clause.Where 转换为 ent 的 Where 函数
-func Where(where clause.Where) func(s *sql.Selector) {
+func Where(where clause.Where, opts ...Option) func(s *sql.Selector) {
+
+	opt := &options{}
+	for _, o := range opts {
+		o(opt)
+	}
+
 	return func(s *sql.Selector) {
 		if len(where.Exprs) == 0 {
 			return
 		}
 
 		// 将 query/clause.Where 转换为 ent 的条件
-		pred, err := convertToEntWhere(where)
+		pred, err := convertToEntWhere(where, opt)
 		if err != nil {
 			s.Builder.AddError(err)
 			return
@@ -28,14 +64,14 @@ func Where(where clause.Where) func(s *sql.Selector) {
 }
 
 // convertToEntWhere 将 query/clause.Where 转换为 ent 的条件
-func convertToEntWhere(where clause.Where) (*sql.Predicate, error) {
+func convertToEntWhere(where clause.Where, opt *options) (*sql.Predicate, error) {
 	if len(where.Exprs) == 0 {
 		return nil, nil
 	}
 
 	var preds []*sql.Predicate
 	for _, expr := range where.Exprs {
-		pred, err := convertToEntPredicate(expr)
+		pred, err := convertToEntPredicate(expr, opt)
 		if err != nil {
 			return nil, err
 		}
@@ -54,28 +90,28 @@ func convertToEntWhere(where clause.Where) (*sql.Predicate, error) {
 }
 
 // convertToEntPredicate 将 query/clause.Expression 转换为 *sql.Predicate
-func convertToEntPredicate(expr clause.Expression) (*sql.Predicate, error) {
+func convertToEntPredicate(expr clause.Expression, opt *options) (*sql.Predicate, error) {
 	switch e := expr.(type) {
 	case clause.Eq:
-		return sql.EQ(e.Column, e.Value), nil
+		return sql.EQ(opt.Column(e.Column), e.Value), nil
 	case clause.Neq:
-		return sql.NEQ(e.Column, e.Value), nil
+		return sql.NEQ(opt.Column(e.Column), e.Value), nil
 	case clause.Gt:
-		return sql.GT(e.Column, e.Value), nil
+		return sql.GT(opt.Column(e.Column), e.Value), nil
 	case clause.Gte:
-		return sql.GTE(e.Column, e.Value), nil
+		return sql.GTE(opt.Column(e.Column), e.Value), nil
 	case clause.Lt:
-		return sql.LT(e.Column, e.Value), nil
+		return sql.LT(opt.Column(e.Column), e.Value), nil
 	case clause.Lte:
-		return sql.LTE(e.Column, e.Value), nil
+		return sql.LTE(opt.Column(e.Column), e.Value), nil
 	case clause.Like:
 		// 将 interface{} 转换为 string
 		if likeValue, ok := e.Value.(string); ok {
-			return sql.Like(e.Column, likeValue), nil
+			return sql.Like(opt.Column(e.Column), likeValue), nil
 		}
 		return nil, errors.New("like value must be string")
 	case clause.IN:
-		return sql.In(e.Column, e.Values...), nil
+		return sql.In(opt.Column(e.Column), e.Values...), nil
 	case clause.AndExpr:
 		// 对于 AND 表达式，递归处理所有子表达式
 		if len(e.Exprs) == 0 {
@@ -84,7 +120,7 @@ func convertToEntPredicate(expr clause.Expression) (*sql.Predicate, error) {
 		// 初始化 AND 条件切片
 		var andPreds []*sql.Predicate
 		for _, subExpr := range e.Exprs {
-			pred, err := convertToEntPredicate(subExpr)
+			pred, err := convertToEntPredicate(subExpr, opt)
 			if err != nil {
 				return nil, err
 			}
@@ -106,7 +142,7 @@ func convertToEntPredicate(expr clause.Expression) (*sql.Predicate, error) {
 		// 初始化 OR 条件切片
 		var orPreds []*sql.Predicate
 		for _, subExpr := range e.Exprs {
-			pred, err := convertToEntPredicate(subExpr)
+			pred, err := convertToEntPredicate(subExpr, opt)
 			if err != nil {
 				return nil, err
 			}
@@ -128,7 +164,7 @@ func convertToEntPredicate(expr clause.Expression) (*sql.Predicate, error) {
 		// 初始化 NOT 条件切片
 		var notPreds []*sql.Predicate
 		for _, subExpr := range e.Exprs {
-			pred, err := convertToEntPredicate(subExpr)
+			pred, err := convertToEntPredicate(subExpr, opt)
 			if err != nil {
 				return nil, err
 			}
@@ -148,7 +184,12 @@ func convertToEntPredicate(expr clause.Expression) (*sql.Predicate, error) {
 }
 
 // OrderBy 将 clause.OrderBys 转换为 ent 的 OrderBy 函数
-func OrderBy(orders clause.OrderBys) func(s *sql.Selector) {
+func OrderBy(orders clause.OrderBys, opts ...Option) func(s *sql.Selector) {
+	opt := &options{}
+	for _, o := range opts {
+		o(opt)
+	}
+
 	return func(s *sql.Selector) {
 		if len(orders) == 0 {
 			return
@@ -156,9 +197,9 @@ func OrderBy(orders clause.OrderBys) func(s *sql.Selector) {
 
 		for _, order := range orders {
 			if order.Desc {
-				s.OrderBy(sql.Desc(order.Column))
+				s.OrderBy(sql.Desc(opt.Column(order.Column)))
 			} else {
-				s.OrderBy(sql.Asc(order.Column))
+				s.OrderBy(sql.Asc(opt.Column(order.Column)))
 			}
 		}
 	}
@@ -179,10 +220,10 @@ func Pagination(pagination clause.Pagination) func(s *sql.Selector) {
 }
 
 // Query 将多个查询组件转换为 ent 的查询函数
-func Query(where clause.Where, orders clause.OrderBys, pagination clause.Pagination) func(s *sql.Selector) {
+func Query(where clause.Where, orders clause.OrderBys, pagination clause.Pagination, opts ...Option) func(s *sql.Selector) {
 	return func(s *sql.Selector) {
-		Where(where)(s)
-		OrderBy(orders)(s)
+		Where(where, opts...)(s)
+		OrderBy(orders, opts...)(s)
 		Pagination(pagination)(s)
 	}
 }
--- adapter/gorm/gorm.go
+++ adapter/gorm/gorm.go
@@ -6,15 +6,51 @@ import (
 	gormClause "gorm.io/gorm/clause"
 )
 
+type options struct {
+	mapper MapperFunc
+}
+
+type Option func(*options)
+
+// MapperFunc 定义了查询列名到 GORM 列名的映射函数类型
+// 输入参数 queryColumn 是查询条件中使用的列名
+// 返回值 gormColumn 是数据库表中的实际列名
+type MapperFunc func(queryColumn string) (gormColumn string)
+
+// WithMapper 创建一个配置选项，用于设置查询列名到数据库实际列名的映射函数
+// mapper 参数是一个 MapperFunc 类型的函数，用于自定义列名映射逻辑
+// 该选项用于在将查询条件转换为 GORM 条件时，通过自定义函数实现灵活的列名映射
+// 例如：可以实现驼峰命名转下划线命名、添加前缀/后缀等复杂映射逻辑
+func WithMapper(mapper MapperFunc) Option {
+	// 返回一个 Option 函数类型，该函数接收一个 options 指针并修改其 mapper 字段
+	return func(o *options) {
+		o.mapper = mapper
+	}
+}
+
+func (o *options) Column(column string) string {
+	if o.mapper != nil {
+		return o.mapper(column)
+	}
+
+	return column
+}
+
 // Where 将 clause.Where 转换为 gorm scope
-func Where(where clause.Where) func(db *gorm.DB) *gorm.DB {
+func Where(where clause.Where, opts ...Option) func(db *gorm.DB) *gorm.DB {
+
+	opt := &options{}
+	for _, o := range opts {
+		o(opt)
+	}
+
 	return func(db *gorm.DB) *gorm.DB {
 		if len(where.Exprs) == 0 {
 			return db
 		}
 
 		// 将 query/clause.Where 转换为 gorm/clause.Where
-		gormWhere := convertWhere(where)
+		gormWhere := convertWhere(where, opt)
 
 		// 将 gorm/clause.Where 给到 gorm.DB 的 Where 函数
 		return db.Where(gormWhere)
@@ -22,11 +58,11 @@ func Where(where clause.Where) func(db *gorm.DB) *gorm.DB {
 }
 
 // convertWhere 将 query/clause.Where 转换为 gorm/clause.Where
-func convertWhere(where clause.Where) gormClause.Where {
+func convertWhere(where clause.Where, opt *options) gormClause.Where {
 	var gormExprs []gormClause.Expression
 
 	for _, expr := range where.Exprs {
-		gormExpr := convertExpr(expr)
+		gormExpr := convertExpr(expr, opt)
 		if gormExpr != nil {
 			gormExprs = append(gormExprs, gormExpr)
 		}
@@ -36,28 +72,28 @@ func convertWhere(where clause.Where) gormClause.Where {
 }
 
 // convertExpr 将 query/clause.Expression 转换为 gorm/clause.Expression
-func convertExpr(expr clause.Expression) gormClause.Expression {
+func convertExpr(expr clause.Expression, opt *options) gormClause.Expression {
 	switch e := expr.(type) {
 	case clause.Eq:
-		return gormClause.Eq{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Eq{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.Neq:
-		return gormClause.Neq{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Neq{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.Gt:
-		return gormClause.Gt{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Gt{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.Gte:
-		return gormClause.Gte{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Gte{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.Lt:
-		return gormClause.Lt{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Lt{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.Lte:
-		return gormClause.Lte{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Lte{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.Like:
-		return gormClause.Like{Column: gormClause.Column{Name: e.Column}, Value: e.Value}
+		return gormClause.Like{Column: gormClause.Column{Name: opt.Column(e.Column)}, Value: e.Value}
 	case clause.IN:
-		return gormClause.IN{Column: gormClause.Column{Name: e.Column}, Values: e.Values}
+		return gormClause.IN{Column: gormClause.Column{Name: opt.Column(e.Column)}, Values: e.Values}
 	case clause.AndExpr:
 		var gormExprs []gormClause.Expression
 		for _, subExpr := range e.Exprs {
-			gormExpr := convertExpr(subExpr)
+			gormExpr := convertExpr(subExpr, opt)
 			if gormExpr != nil {
 				gormExprs = append(gormExprs, gormExpr)
 			}
@@ -69,7 +105,7 @@ func convertExpr(expr clause.Expression) gormClause.Expression {
 	case clause.OrExpr:
 		var gormExprs []gormClause.Expression
 		for _, subExpr := range e.Exprs {
-			gormExpr := convertExpr(subExpr)
+			gormExpr := convertExpr(subExpr, opt)
 			if gormExpr != nil {
 				gormExprs = append(gormExprs, gormExpr)
 			}
@@ -81,7 +117,7 @@ func convertExpr(expr clause.Expression) gormClause.Expression {
 	case clause.NotExpr:
 		var gormExprs []gormClause.Expression
 		for _, subExpr := range e.Exprs {
-			gormExpr := convertExpr(subExpr)
+			gormExpr := convertExpr(subExpr, opt)
 			if gormExpr != nil {
 				gormExprs = append(gormExprs, gormExpr)
 			}
@@ -96,7 +132,12 @@ func convertExpr(expr clause.Expression) gormClause.Expression {
 }
 
 // OrderBy 将 clause.OrderBys 转换为 gorm scope
-func OrderBy(orders clause.OrderBys) func(db *gorm.DB) *gorm.DB {
+func OrderBy(orders clause.OrderBys, opts ...Option) func(db *gorm.DB) *gorm.DB {
+	opt := &options{}
+	for _, o := range opts {
+		o(opt)
+	}
+
 	return func(db *gorm.DB) *gorm.DB {
 		if len(orders) == 0 {
 			return db
@@ -106,7 +147,7 @@ func OrderBy(orders clause.OrderBys) func(db *gorm.DB) *gorm.DB {
 
 		for _, order := range orders {
 			gOrderByCols = append(gOrderByCols, gormClause.OrderByColumn{
-				Column: gormClause.Column{Name: order.Column},
+				Column: gormClause.Column{Name: opt.Column(order.Column)},
 				Desc:   order.Desc,
 			})
 		}
@@ -131,10 +172,10 @@ func Pagination(pagination clause.Pagination) func(db *gorm.DB) *gorm.DB {
 }
 
 // Query 将多个查询组件转换为 gorm scope
-func Query(where clause.Where, orders clause.OrderBys, pagination clause.Pagination) func(db *gorm.DB) *gorm.DB {
+func Query(where clause.Where, orders clause.OrderBys, pagination clause.Pagination, opts ...Option) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		db = Where(where)(db)
-		db = OrderBy(orders)(db)
+		db = Where(where, opts...)(db)
+		db = OrderBy(orders, opts...)(db)
 		db = Pagination(pagination)(db)
 		return db
 	}
